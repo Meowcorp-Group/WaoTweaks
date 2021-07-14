88858df9565eafd9d414c6a53b1dc2168eb60268
Waoweens <30313089+Waoweens@users.noreply.github.com>
2021-07-14 11:38:01 +0700

    addded easter egg item :)

--- src/main/java/online/meowcorp/waotweaks/EasterEggs.java
+++ src/main/java/online/meowcorp/waotweaks/EasterEggs.java
@@ -1,6 +1,8 @@
 package online.meowcorp.waotweaks;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolItem;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Rarity;
 import net.minecraft.util.registry.Registry;
@@ -16,10 +18,21 @@ public class EasterEggs {
                     .rarity(Rarity.UNCOMMON)
     );
 
+    //God's golden sword
+    public static ToolItem GODS_GOLDEN_SWORD = new SwordItem(
+            GodSwordMaterial.INSTANCE, 2147483647, 2147483647.0F,
+            new Item.Settings()
+            .fireproof()
+            .maxDamage(-2147483647)
+    );
+
     public static void register() {
 
         //NINETEEN DOLLAR FORTNITE CARD
         Registry.register(Registry.ITEM, new Identifier("waotweaks", "fortnite_card"), FORTNITE_CARD);
+
+        //God's golden sword
+        Registry.register(Registry.ITEM, new Identifier("waotweaks", "gods_golden_sword"), GODS_GOLDEN_SWORD);
 
     }
 }
---
+++ src/main/java/online/meowcorp/waotweaks/GodSwordMaterial.java
@@ -0,0 +1,41 @@
+package online.meowcorp.waotweaks;
+
+import net.minecraft.item.Items;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.recipe.Ingredient;
+
+public class GodSwordMaterial implements ToolMaterial {
+
+    public static final GodSwordMaterial INSTANCE = new GodSwordMaterial();
+
+    @Override
+    public int getDurability() {
+        return 2147483647;
+    }
+
+    @Override
+    public float getMiningSpeedMultiplier() {
+        return 2147483647.0F;
+    }
+
+    @Override
+    public float getAttackDamage() {
+        return 0.0F;
+    }
+
+    @Override
+    public int getMiningLevel() {
+        return 2147483647;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return 2147483647;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return Ingredient.ofItems(Items.GOLD_INGOT);
+    }
+
+}
